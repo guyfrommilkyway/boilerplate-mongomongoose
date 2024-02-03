de7a4c88a7d5ba244be7f79052fa12e1456d3ff1
Almer Tampus <almerflorestampus@gmail.com>
2024-02-03 19:44:18 +0800

    Add queryChain

--- myApp.js
+++ myApp.js
@@ -114,7 +114,15 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
 	const foodToSearch = 'burrito';
 
-	done(null /*, data*/);
+	Person.find({ favoriteFoods: foodToSearch })
+		.sort({ name: 1 })
+		.limit(2)
+		.select('-age')
+		.exec((err, data) => {
+			if (err) console.log(err);
+
+			done(null, data);
+		});
 };
 
 /** **Well Done !!**
